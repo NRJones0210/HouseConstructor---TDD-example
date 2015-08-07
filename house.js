f524f9e2f004737bcbbc3128242d76b82643da37
Nick Jones <NRJones0210@gmail.com>
2015-08-07 13:52:56 -0600

    Working. Needs refactored

--- house.js
+++ house.js
@@ -1,5 +1,23 @@
-function House (numberOfDoors) {
-  this.numberOfDoors = numberOfDoors,
-}
+function House (numberOfDoors, numberOfWindows) {
+  this.numberOfDoors = numberOfDoors
+  this.numberOfWindows = numberOfWindows
 
+  this.toString = function(numberOfDoors,numberOfWindows) {
+    if (this.numberOfDoors > 1 && this.numberOfWindows === 1) {
+      return 'The house has ' + this.numberOfDoors + ' doors and ' + this.numberOfWindows + ' window'
+    }
+    else if (this.numberOfDoors > 1 && this.numberOfWindows > 1) {
+      return 'The house has ' + this.numberOfDoors + ' doors and ' + this.numberOfWindows + ' windows'
+    }
+    else if (this.numberOfDoors === 1 && this.numberOfWindows > 1) {
+      return 'The house has ' + this.numberOfDoors + ' door and ' + this.numberOfWindows + ' windows'
+    }
+    else if (this.numberOfDoors === 1 && this.numberOfWindows === 1) {
+      return 'The house has ' + this.numberOfDoors + ' door and ' + this.numberOfWindows + ' window'
+    }
+    else if (this.numberOfDoors === 0 && this.numberOfWindows === 0) {
+      return 'The house has ' + this.numberOfDoors + ' doors and ' + this.numberOfWindows + ' windows'
+    }
+  }
+}
 module.exports = House;
